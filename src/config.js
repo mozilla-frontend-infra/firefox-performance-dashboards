fdec2bdd8cbc53c3142bef348833175472143e8b
Armen Zambrano G <armenzg@gmail.com>
2018-07-31 11:28:21 -0400

    Issue #2 - Initial MVP layout
    Fetch and plot AWFY subbenchmarks. Future iterations will compare Firefox and Chrome.
    
    Use perf-goggles to fetch Perfherder data and give it to react-metrics-graphics
    to plot all subbenchmarks.

--- src/config.js
+++ src/config.js
@@ -1,25 +1,51 @@
-const BENCHMARKS = [
-  'ARES6',
-  'JetStream',
-  'motionmark_htmlsuite',
-  'motionmark_animometer',
-];
+const RAPTOR_OPT_OPTIONS = { frameworkId: 10, buildType: 'opt', extraOptions: [] };
+const RAPTOR_PGO_OPTIONS = { frameworkId: 10, buildType: 'pgo', extraOptions: [] };
+
+const BENCHMARKS = {
+  'raptor-motionmark-animometer-firefox': {
+    label: 'MotionMark Animometer',
+  },
+  'raptor-motionmark-htmlsuite-firefox': {
+    label: 'MotionMark HtmlSuite',
+  },
+  'raptor-speedometer-firefox': {
+    label: 'Speedometer',
+  },
+  'raptor-stylebench-firefox': {
+    label: 'StyleBench',
+  },
+  'raptor-sunspider-firefox': {
+    label: 'SunSpider',
+  },
+  'raptor-webaudio-firefox': {
+    label: 'WebAudio',
+  },
+};
 
 const CONFIG = {
   linux64: {
     label: 'Linux 64bit',
     perfherderKey: 'linux64',
     benchmarks: BENCHMARKS,
+    options: RAPTOR_PGO_OPTIONS,
+  },
+  mac: {
+    label: 'Mac OS X',
+    perfherderKey: 'osx-10-10',
+    benchmarks: BENCHMARKS,
+    options: RAPTOR_OPT_OPTIONS,
   },
   win7: {
     label: 'Windows 7 32bit',
     perfherderKey: 'windows7-32',
     benchmarks: BENCHMARKS,
+    options: RAPTOR_PGO_OPTIONS,
   },
   win10: {
     label: 'Windows 10 64bit',
     perfherderKey: 'windows10-64',
     benchmarks: BENCHMARKS,
+    options: RAPTOR_PGO_OPTIONS,
   },
 };
 
