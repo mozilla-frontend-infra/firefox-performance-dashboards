2d83eece70777af010d5ab46b8b66f00f998a638
Armen Zambrano G <armenzg@gmail.com>
2018-08-20 10:19:35 -0400

    Move routes.jsx logic to app/index.jsx

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,4 +1,24 @@
-import Routes from '../routes';
+import { parse } from 'query-string';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import Benchmark from '../views/Benchmark';
 import './metricsGraphics.css';
 
-export default () => <Routes />;
+export default () => (
+  <Switch>
+    <Route
+      path="/"
+      render={({ location }) => {
+        const { platform, benchmark } = parse(location.search);
+        if (!platform || !benchmark) {
+          return <Redirect to="/?platform=win10&benchmark=motionmark-animometer" />;
+        }
+        return (
+          <Benchmark
+            platform={platform}
+            benchmark={benchmark}
+          />
+        );
+      }}
+    />
+  </Switch>
+);
