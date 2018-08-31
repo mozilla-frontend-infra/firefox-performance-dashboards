496a1cb070f59a825b85f74e6f67e6cc106bb615
Armen Zambrano G <armenzg@gmail.com>
2018-08-31 08:51:29 -0400

    Split off fetching of data into its own module

--- src/views/Benchmark/index.jsx
+++ src/views/Benchmark/index.jsx
@@ -1,10 +1,9 @@
 import { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import { fetchBenchmarkData, subbenchmarksData } from '@mozilla-frontend-infra/perf-goggles';
 import Header from '../../components/Header';
 import Graphs from '../../components/Graphs';
-import { BENCHMARKS, CONFIG } from '../../config';
-import prepareData from '../../utils/prepareData';
+import { CONFIG } from '../../config';
+import fetchData from '../../utils/fetchData';
 
 const validCombination = (platform, benchmark) => (
   benchmark === 'overview' ||
@@ -72,38 +71,7 @@ export class Benchmark extends Component {
   }
 
   async fetchData(platform, benchmark) {
-    const benchmarkData = {};
-    if (benchmark === 'overview') {
-      const benchmarksToCompare = CONFIG.platforms[platform].benchmarks;
-      await Promise.all(benchmarksToCompare
-        .map(async (benchmarkKey) => {
-          const comparingBenchmarks = Object.keys(BENCHMARKS[benchmarkKey].compare);
-          await Promise.all(comparingBenchmarks
-            .map(async (modeKey) => {
-              const benchmarkOptions = BENCHMARKS[benchmarkKey].compare[modeKey];
-              benchmarkData[benchmarkOptions.suite] = await fetchBenchmarkData(
-                benchmarkOptions.frameworkId,
-                CONFIG.platforms[platform].platform,
-                benchmarkOptions.suite,
-                benchmarkOptions.buildType,
-              );
-              benchmarkData[benchmarkOptions.suite].configUID = benchmarkKey;
-            }));
-        }));
-    } else {
-      await Promise.all(Object.keys(BENCHMARKS[benchmark].compare)
-        .map(async (modeKey) => {
-          const benchmarkOptions = BENCHMARKS[benchmark].compare[modeKey];
-          benchmarkData[benchmarkOptions.suite] = await subbenchmarksData(
-            benchmarkOptions.frameworkId,
-            CONFIG.platforms[platform].platform,
-            benchmarkOptions.suite,
-            benchmarkOptions.buildType,
-          );
-          benchmarkData[benchmarkOptions.suite].configUID = benchmark;
-        }));
-    }
-    this.setState({ benchmarkData: prepareData(benchmarkData) });
+    this.setState({ benchmarkData: fetchData(platform, benchmark) });
   }
 
   render() {
