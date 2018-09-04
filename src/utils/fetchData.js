fcae28276dc2b4735436ee2788a3fec337236d21
Armen Zambrano G <armenzg@gmail.com>
2018-09-04 14:24:15 -0400

    Issue #38 - Show parental main score on the drilldown view

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -3,33 +3,14 @@ import { BENCHMARKS, CONFIG } from '../config';
 import prepareData from './prepareData';
 
 const fetchData = async (platform, benchmark) => {
-  const benchmarkData = {};
-  if (benchmark === 'overview') {
-    const benchmarksToCompare = CONFIG.platforms[platform].benchmarks;
-    await Promise.all(benchmarksToCompare
-      .map(async (benchmarkKey) => {
-        const comparingBenchmarks = Object.keys(BENCHMARKS[benchmarkKey].compare);
-        await Promise.all(comparingBenchmarks
-          .map(async (modeKey) => {
-            const benchmarkOptions = BENCHMARKS[benchmarkKey].compare[modeKey];
-            const data = await fetchBenchmarkData(
-              benchmarkOptions.frameworkId,
-              CONFIG.platforms[platform].platform,
-              benchmarkOptions.suite,
-              benchmarkOptions.buildType,
-              benchmarkOptions.extraOptions,
-            );
-            if (data) {
-              benchmarkData[benchmarkOptions.suite] = data;
-              benchmarkData[benchmarkOptions.suite].configUID = benchmarkKey;
-            }
-          }));
-      }));
-  } else {
-    await Promise.all(Object.keys(BENCHMARKS[benchmark].compare)
+  const ALL_DATA = {};
+
+  const fetchIt = async (fetchMethod, configUID) => {
+    const comparingBenchmarks = Object.keys(BENCHMARKS[configUID].compare);
+    return Promise.all(comparingBenchmarks
       .map(async (modeKey) => {
-        const benchmarkOptions = BENCHMARKS[benchmark].compare[modeKey];
-        const data = await subbenchmarksData(
+        const benchmarkOptions = BENCHMARKS[configUID].compare[modeKey];
+        const data = await fetchMethod(
           benchmarkOptions.frameworkId,
           CONFIG.platforms[platform].platform,
           benchmarkOptions.suite,
@@ -37,12 +18,31 @@ const fetchData = async (platform, benchmark) => {
           benchmarkOptions.extraOptions,
         );
         if (data) {
-          benchmarkData[benchmarkOptions.suite] = data;
-          benchmarkData[benchmarkOptions.suite].configUID = benchmark;
+          const { color, label } = BENCHMARKS[configUID].compare[benchmarkOptions.suite];
+          const key = (fetchMethod === fetchBenchmarkData)
+            ? `overview-${benchmarkOptions.suite}` : benchmarkOptions.suite;
+          ALL_DATA[key] = {
+            ...data,
+            configUID,
+            suite: benchmarkOptions.suite,
+            color,
+            label: fetchMethod === fetchBenchmarkData ? benchmarkOptions.label : label,
+          };
         }
       }));
+  };
+
+  if (benchmark === 'overview') {
+    const benchmarksToCompare = CONFIG.platforms[platform].benchmarks;
+    await Promise.all(benchmarksToCompare
+      .map(async (configUID) => {
+        await fetchIt(fetchBenchmarkData, configUID);
+      }));
+  } else {
+    await fetchIt(fetchBenchmarkData, benchmark);
+    await fetchIt(subbenchmarksData, benchmark);
   }
-  return prepareData(benchmarkData);
+  return prepareData(ALL_DATA);
 };
 
 export default fetchData;
