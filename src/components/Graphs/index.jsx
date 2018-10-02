33c24e863eb150558e26347be07108bb13f80801
Joyce Echessa <jokhessa@yahoo.com>
2018-10-02 12:43:12 +0300

    Use mdi-react package for svg icon

--- src/components/Graphs/index.jsx
+++ src/components/Graphs/index.jsx
@@ -1,24 +1,13 @@
+import PoundIcon from 'mdi-react/PoundIcon';
 import LinkIcon from '@material-ui/icons/Link';
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import Legend from '../../components/Legend';
 import ChartJSWrapper from '../../components/ChartJSWrapper';
 
-const styles = {
-  anchorTag: {
-    marginRight: '3px',
-  },
-  anchorIcon: {
-    verticalAlign: 'middle',
-  },
-};
-
 const sortOverviewFirst = (a, b) => {
-  if (a.includes('overview')) {
+  if (a.includes('overview') || b.includes('overview')) {
     return -1;
-  }
-  if (b.includes('overview')) {
-    return 1;
   }
   return (a <= b ? -1 : 1);
 };
@@ -30,6 +19,10 @@ const styles = () => ({
   },
   linkIcon: {
     marginBottom: -5,
+  },
+  poundIcon: {
+    color: '#696969',
+    textDecoration: 'none',
   },
 });
 
@@ -55,21 +48,18 @@ const Graphs = ({ classes, benchmarkData, overviewMode }) => (
       .sort(overviewMode ? undefined : sortOverviewFirst)
       .map((key) => {
         const graph = benchmarkData.graphs[key];
-        graph.anchor = key;
+        graph.anchorId = key;
         return graph;
       })
       .map(({
-        chartJsData, chartJsOptions, jointUrl, title, anchor,
+        chartJsData, chartJsOptions, jointUrl, title, anchorId,
       }) => (
         <div key={title}>
-          <h2>
-            <a id={anchor} href={`#${anchor}`} style={styles.anchorTag}>
-              <svg width="20" height="20" viewBox="0 0 24 24" style={styles.anchorIcon}>
-                <path fill="#000000" d="M5.41,21L6.12,17H2.12L2.47,15H6.47L7.53,9H3.53L3.88,7H7.88L8.59,3H10.59L9.88,7H15.88L16.59,3H18.59L17.88,7H21.88L21.53,9H17.53L16.47,15H20.47L20.12,17H16.12L15.41,21H13.41L14.12,17H8.12L7.41,21H5.41M9.53,9L8.47,15H14.47L15.53,9H9.53Z" />
-              </svg>
-            </a>
+          <h2 className={classes.benchmarkTitle}>
+            <a id={anchorId} href={`#${anchorId}`}><PoundIcon className={classes.poundIcon} size={16} /></a>
             {title}
           </h2>
+          <a href={jointUrl} target="_blank" rel="noopener noreferrer"><LinkIcon className={classes.linkIcon} /></a>
           <ChartJSWrapper chartJsData={chartJsData} chartJsOptions={chartJsOptions} />
         </div>
       ))}
