764aae9df53d9164934313be6fdea290b516cfdf
Aimen <aimenbatool72@gmail.com>
2018-10-23 20:04:43 +0500

    Issue #82 - Update test for pickers component

--- test/components/Pickers.test.jsx
+++ test/components/Pickers.test.jsx
@@ -3,6 +3,25 @@ import renderer from 'react-test-renderer';
 import 'raf/polyfill';
 import Pickers from '../../src/components/Pickers';
 
+const benchmarkOptions = [
+  { value: 'overview', label: 'Overview' },
+  { value: 'kraken', label: 'Kraken' },
+  { value: 'motionmark-animometer', label: 'MotionMark Animometer' },
+  { value: 'motionmark-htmlsuite', label: 'MotionMark HtmlSuite' },
+  { value: 'speedometer', label: 'Speedometer' },
+  { value: 'stylebench', label: 'StyleBench' },
+  { value: 'sunspider', label: 'SunSpider' },
+  { value: 'webaudio', label: 'WebAudio' },
+];
+
+const platformOptions = [
+  { value: 'linux64', label: 'Linux 64bit' },
+  { value: 'mac', label: 'Mac OS X' },
+  { value: 'win7', label: 'Windows 7 32bit' },
+  { value: 'win10', label: 'Windows 10 64bit' },
+  { value: 'android', label: 'Android' },
+];
+
 it('renders correctly', () => {
   const tree = renderer
     .create((
@@ -10,6 +29,8 @@ it('renders correctly', () => {
         platform="win10"
         benchmark="overview"
         onChange={() => {}}
+        benchmarkOptions={benchmarkOptions}
+        platformOptions={platformOptions}
       />
     ))
     .toJSON();
