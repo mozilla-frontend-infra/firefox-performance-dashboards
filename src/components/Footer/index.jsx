1c4376c7222a8cb791343c30532a7c1bb687d89e
Luke Barrett <gotluke@gmail.com>
2018-10-24 15:52:45 -0600

    Styled footer link

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 
-const Footer = () => <a href="https://github.com/mozilla-frontend-infra/firefox-performance-dashboard/issues">Open a issue.</a>;
 
-export default Footer;
+const styles = () => ({
+  link: {
+    color: '#868686',
+    textDecoration: 'none',
+    fontSize: '13px',
+  },
+});
+
+const Footer = ({ classes }) => <a className={classes.link} href="https://github.com/mozilla-frontend-infra/firefox-performance-dashboard/issues">Open a issue</a>;
+
+Footer.propTypes = ({
+  classes: PropTypes.shape({}).isRequired,
+});
+
+export default withStyles(styles)(Footer);
