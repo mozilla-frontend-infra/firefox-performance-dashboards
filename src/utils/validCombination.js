c0d57709073fdda557da1bfce68963bfef28a2b6
Armen Zambrano G <armenzg@gmail.com>
2018-11-06 14:02:37 -0500

    Refactor fetchData to make it easier to understand
    There are other minor changes.

--- src/utils/validCombination.js
+++ src/utils/validCombination.js
@@ -1,9 +1,9 @@
 import { CONFIG, TIMERANGE_UPPER_LIMIT } from '../config';
 
 const validCombination = (platform, benchmark, timeRange) => (
-  CONFIG.platforms[platform] && (
+  CONFIG.views[platform] && (
     benchmark === 'overview'
-    || CONFIG.platforms[platform].benchmarks.includes(benchmark)
+    || CONFIG.views[platform].benchmarks.includes(benchmark)
   ) && (
     !!Math.round(timeRange) && parseInt(timeRange, 10) <= TIMERANGE_UPPER_LIMIT
   )
