3afa267be5dd24c2f04aee4509c75997f04a450f
Sid Vishnoi <sidvishnoi8@gmail.com>
2019-08-07 23:41:18 +0530

    Fix charts resizing unnecessarily

--- src/components/ChartJSWrapper/index.jsx
+++ src/components/ChartJSWrapper/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Chart from 'react-chartjs-2';
+import { withStyles } from '@material-ui/core/styles';
 
-const ChartJSWrapper = ({ chartJsData, chartJsOptions }) => (
-  <Chart
-    type="scatter"
-    data={chartJsData}
-    height={110}
-    options={chartJsOptions}
-  />
+const styles = () => ({
+  chartJSWrapper: {
+    height: '30vmax',
+    maxHeight: '18rem',
+    paddingRight: '1rem',
+  },
+});
+
+const ChartJSWrapper = ({ classes, chartJsData, chartJsOptions }) => (
+  <div className={classes.chartJSWrapper}>
+    <Chart type="scatter" data={chartJsData} options={chartJsOptions} />
+  </div>
 );
 
 ChartJSWrapper.propTypes = {
@@ -16,4 +22,4 @@ ChartJSWrapper.propTypes = {
   chartJsOptions: PropTypes.shape({}).isRequired,
 };
 
-export default ChartJSWrapper;
+export default withStyles(styles)(ChartJSWrapper);
