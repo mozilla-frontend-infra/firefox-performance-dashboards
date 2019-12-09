132ee6202a2c99bb6676b1ac4d83bd10c74f200b
Divyanshu <divyanshu.r46956@gmail.com>
2019-12-09 23:51:36 +0100

    Added support for sentry.

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,10 @@
+import Raven from 'raven-js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './components/Router';
+
+Raven.config(
+  'https://7d4b85013ba648c0bd32ce069fde5755@sentry.io/1851579',
+).install();
 
 ReactDOM.render(<Router />, document.getElementById('root'));
