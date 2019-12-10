8108c1c07ab96e466e2ac5ce22694f9b9ed7cc43
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2019-12-10 22:44:39 +0100

    Add Sentry support (#258)

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,10 @@
+import Raven from 'raven-js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './components/Router';
+
+Raven.config(
+  'https://a46514ae1f2d42cd83c10d1bb8f4141e@sentry.prod.mozaws.net/436',
+).install();
 
 ReactDOM.render(<Router />, document.getElementById('root'));
