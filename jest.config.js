7e312d3760f755b92b57188a935f4a0e4ff9f57b
Dragana Damjanovic <dragana.damjano@gmail.com>
2020-10-21 22:54:37 +0200

    Use h3 as default version.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 const neutrino = require('neutrino');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'test';
-process.env.DASHBOARD = process.env.DASHBOARD || 'awfy';
+process.env.DASHBOARD = process.env.DASHBOARD || 'h3';
 
 module.exports = neutrino().jest();
