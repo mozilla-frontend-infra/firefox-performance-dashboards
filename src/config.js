0eebb97d1b0a3b493686ea6c0c52111110bdcfaa
Dragana Damjanovic <dragana.damjano@gmail.com>
2020-10-21 23:27:26 +0200

    Configure only h3

--- src/config.js
+++ src/config.js
@@ -2,14 +2,7 @@ import awfy from './awfy';
 import awsy from './awsy';
 import h3 from './h3';
 
-let config = {};
-if (process.env.DASHBOARD === 'awfy') {
-  config = { ...awfy };
-} else if (process.env.DASHBOARD === 'awsy') {
-  config = { ...awsy };
-} else if (process.env.DASHBOARD === 'h3') {
-  config = { ...h3 };
-}
+let config = { ...h3 };
 
 const {
   queryInfo, BENCHMARKS, CONFIG, TIMERANGE_UPPER_LIMIT,
