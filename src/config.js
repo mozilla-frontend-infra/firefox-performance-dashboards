6b0c864cd68ecbcf832b58b117d0cf2bc8e6372b
Dragana Damjanovic <dragana.damjano@gmail.com>
2020-10-21 20:39:21 +0200

    Add the http3 dashboard

--- src/config.js
+++ src/config.js
@@ -1,11 +1,14 @@
 import awfy from './awfy';
 import awsy from './awsy';
+import h3 from './h3';
 
 let config = {};
 if (process.env.DASHBOARD === 'awfy') {
   config = { ...awfy };
 } else if (process.env.DASHBOARD === 'awsy') {
   config = { ...awsy };
+} else if (process.env.DASHBOARD === 'h3') {
+  config = { ...h3 };
 }
 
 const {
