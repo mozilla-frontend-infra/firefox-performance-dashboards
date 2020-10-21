6c092ef0a143871e30a0fb23c249585cb9b5e0a0
Dragana Damjanovic <dragana.damjano@gmail.com>
2020-10-21 23:56:05 +0200

    Remove imports that are not needed

--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
-import awfy from './awfy';
-import awsy from './awsy';
 import h3 from './h3';
 
-let config = { ...h3 };
+const config = { ...h3 };
 
 const {
   queryInfo, BENCHMARKS, CONFIG, TIMERANGE_UPPER_LIMIT,
