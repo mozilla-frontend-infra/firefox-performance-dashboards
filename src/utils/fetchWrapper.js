a4b54aaf3beb524fab9b1b58cbcb7939ba53b3f1
Alex Capalneanu <alexandru.capalneanu@softvision.com>
2020-10-29 15:01:20 +0000

    fetch wrapper to elimitate duplicate requests

---
+++ src/utils/fetchWrapper.js
@@ -0,0 +1,29 @@
+export const promiseCache = {};
+/**
+ * Simple wrapper over fetch, for avoiding duplicate API calls
+ * @param url
+ */
+const fetchWrapper = (url) => {
+  if (promiseCache[url]) {
+    return promiseCache[url].then((res) => res.clone());
+  }
+
+  const fetchPromise = fetch(url);
+
+  promiseCache[url] = fetchPromise
+    .then((res) => {
+      // once resolved, can be removed
+      delete promiseCache[url];
+      return res;
+    })
+
+    .catch((err) => {
+      delete promiseCache[url];
+      throw err;
+    });
+
+  // required to clone the Response object, to allow it to be reused
+  return promiseCache[url].then((res) => res.clone());
+};
+
+export default fetchWrapper;
