8411a7de2f306904dd1291c8e954fc682fd47b45
Carla Severe <csevere@mozilla.com>
2025-01-16 17:52:43 -0800

    simplified and fixed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,30 @@
 const path = require('path');
 const webpack = require('webpack');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: path.resolve(__dirname, '..', './src/index.jsx'),
+const config = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/index.jsx'),
   resolve: {
     extensions: ['.js', '.jsx'],
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    port: 5000,
+    historyApiFallback: true,
   },
   module: {
     rules: [
       {
         // should use babel-loader for all js and jsx files
-        test: /\.(ts|js)x?$/,
+        test: /\.jsx?$/, // Match .js and .jsx files
         exclude: /node_modules/,
         use: [
           {
             loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react'], // For JSX
+            },
           },
         ],
       },
@@ -32,20 +40,30 @@ module.exports = {
       },
       {
         test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: 'asset/inline',
+        type: 'asset',
       },
     ],
   },
-  output: {
-    path: path.resolve(__dirname, '..', './build'),
-    filename: '[name].[chunkhash].js',
-  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '..', './public/index.html'),
+      template: path.resolve(__dirname, './public/index.html'),
     }),
     new webpack.DefinePlugin({
       'process.env.REACT_APP_DASHBOARD': JSON.stringify('awfy'),
     }),
   ],
+};
+
+module.exports = (env) => {
+  if (env.env === 'development') {
+    config.mode = 'development';
+    config.devtool = 'eval-source-map';
+  }
+
+  if (env.env === 'production') {
+    config.mode = 'production';
+    config.devtool = 'source-map';
+  }
+
+  return config;
 };
--- webpack/webpack.development.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mode: 'development',
-  devtool: 'cheap-module-source-map',
-  devServer: {
-    port: 5000,
-    historyApiFallback: true,
-  },
-};
